281f646bc08985546653247146518e2cd9170c30
Thales Felippe <thales_so@hotmail.com>
2022-08-25 19:14:07 -0300

    work

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,7 +1,9 @@
 import { Container } from "react-bootstrap";
 import levelup from '../assets/img/LevelUp-AppLandingPage.png';
+import virtualpokedex from '../assets/img/VirtualPokedex.png';
 import { BsGithub } from "react-icons/bs";
 import { BsArrowUpRightSquareFill } from "react-icons/bs";
+import { BsArrowUpLeftSquareFill } from "react-icons/bs";
 
 export const Work = () => {
     return (
@@ -35,7 +37,38 @@ export const Work = () => {
                         <div className="work-image-right">
                             <a href="https://thalesfelippe.github.io/LevelUp-app-page/" alt="Website">
                                 <div className="image-project-right">
-                                    <img src={levelup} alt="Book" />
+                                    <img src={levelup} alt="Project Image" />
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+                <ul className="work-list">
+                    <li className="featured-work-left">
+                        <div className="work-content-left">
+                            <div>
+                                <p className="work-overline-left">Featured Project</p>
+                                <h3 className="work-title-left">
+                                    <a href="https://thalesfelippe.github.io/PokedexJS/" alt="Website">Virtual Pokedex</a>
+                                </h3>
+                                <div className="work-description-left">
+                                    <p>Pokedex JS was developed from scratch in order to improve knowledge in JavaScript and CSS, using a pokemon api</p>
+                                </div>
+                                <ul className="technology-list-left">
+                                    <li>HTML5</li>
+                                    <li>CSS3</li>
+                                    <li>JavaScript</li>
+                                </ul>
+                                <div className="work-links-left">
+                                <a href="https://thalesfelippe.github.io/PokedexJS/" alt="Website"><BsArrowUpLeftSquareFill/></a>
+                                    <a href="https://github.com/thalesfelippe/PokedexJS" alt="GitHub"><BsGithub/></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="work-image-left">
+                            <a href="https://thalesfelippe.github.io/PokedexJS/" alt="Website">
+                                <div className="image-project-left">
+                                    <img src={virtualpokedex} alt="project image" />
                                 </div>
                             </a>
                         </div>
