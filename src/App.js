82d391360349ebfcd99ebe9f8712a4ac323d3e4f
Thales Felippe <thales_so@hotmail.com>
2022-09-07 00:18:29 -0300

    load screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import RingLoader from "react-spinners/RingLoader";
 import Particle from './Components/Particle';
 import { NavBar } from './Components/NavBar';
 import { Intro } from './Components/Intro';
@@ -10,16 +12,40 @@ import { Footer } from './Components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000)
+  }, []);
+
   return (
     <div className="App">
-      <Particle />
-      <NavBar />
-      <Intro />
-      <About />
-      <Experience />
-      <Work />
-      <Contact />
-      <Footer />
+      {
+        loading ? (
+          <div className='Loader'>
+            <Particle />
+            <RingLoader 
+            color="#36d7b7"
+            loading={loading}
+            />
+          </div>
+
+        ) : (
+          <div className="main">
+            <Particle />
+            <NavBar />
+            <Intro />
+            <About />
+            <Experience />
+            <Work />
+            <Contact />
+            <Footer />
+          </div>
+        )
+      }
     </div>
   );
 }
