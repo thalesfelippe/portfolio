735a9536b95183cae9d566f85e1bc5000f05fef1
Thales Felippe <thales_so@hotmail.com>
2022-09-07 22:33:05 -0300

    ajuste no envio de email

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,7 +10,7 @@ export const Contact = () => {
                 <div className="contact-inner">
                     <h4>I'm looking for new opportunities, if 
                     you're interested, feel free to say hi</h4>
-                    <button onClick={() => window.open("https://mail.google.com/mail/u/0/?fs=1&to=thales.dev.flp@gmail.com&su=I%20arrived%20through%20your%20portfolio&body=Hi&bcc=&tf=cm", "Popup")} className="btn btn-blue">Say Hi</button>
+                    <a className="btn btn-blue" href="mailto:thales.dev.flp@gmail.com" target="_blank" rel="noreferrer" alt="Email" onClick={() => window.open("https://mail.google.com/mail/u/0/?fs=1&to=thales.dev.flp@gmail.com&su=I%20arrived%20through%20your%20portfolio&body=Hi&bcc=&tf=cm", "Popup")}>Say Hi</a>
                 </div>
             </Container>
         </section>
