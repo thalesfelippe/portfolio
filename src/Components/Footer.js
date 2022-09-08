6c30178af21c9c902da11523e351b97b04bca43f
Thales Felippe <thales_so@hotmail.com>
2022-09-07 21:47:06 -0300

    email linkado

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,7 +9,7 @@ export const Footer = () => {
             <Container className="footer-box">
                 <div className="footer-socials">
                     <a href="https://github.com/thalesfelippe/" target="_blank" rel="noreferrer" alt="GitHub"><BsGithub/></a>
-                    <a href="mailto:thales.dev.flp@gmail.com" target="_blank" rel="noreferrer" alt="Email"><BsFillEnvelopeFill/></a>
+                    <a href="mailto:thales.dev.flp@gmail.com" target="_blank" rel="noreferrer" alt="Email" onClick={() => window.open("https://mail.google.com/mail/u/0/?fs=1&to=thales.dev.flp@gmail.com&su=I%20arrived%20through%20your%20portfolio&body=Hi&bcc=&tf=cm", "Popup")}><BsFillEnvelopeFill/></a>
                     <a href="https://www.linkedin.com/in/thales-felippe/" target="_blank" rel="noreferrer" alt="Linkedin"><BsLinkedin/></a>
                 </div>
                 <div className="footer-copyright">
